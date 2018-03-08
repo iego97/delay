9e6a8ff1e98b4f9514dda76992756d5230acd654
alumno <alumno@D3B09>
2018-03-08 10:45:43 -0700

    Add project files.

---
+++ Efectos/Delay.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efectos
+{
+    class Delay : ISampleProvider
+    {
+        int offsetTiempoMS;
+        private ISampleProvider fuente;
+
+
+        public Delay(ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+            offsetTiempoMS = 1000;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+               return fuente.WaveFormat;
+            }
+        }
+
+        //Offset, numero de muestras leídas
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var read = fuente.Read(buffer, offset, count);
+            float tiempoTranscurrido = (float)offset / (float)fuente.WaveFormat.SampleRate;
+
+            float tiempoTranscurridoMS = tiempoTranscurrido * 1000;
+            int numMuestrasOffsetTiempo = (int)(((float)offsetTiempoMS / 1000.0f) * (float)fuente.WaveFormat.SampleRate);
+
+            if (tiempoTranscurridoMS > offsetTiempoMS)
+            {
+                for (int i = 0; i < read; i++)
+                {
+                    buffer[offset + 1] += buffer[offset+i-numMuestrasOffsetTiempo];
+                }
+            }
+            return read;
+        }
+    }
+}
