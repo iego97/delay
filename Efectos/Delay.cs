820e127502d83a1d5ed0be409dd726e1e7e84daa
Iram Garcia <Iram Garcia@DESKTOP-4EOBFD6>
2018-03-14 09:45:20 -0700

    Delay segundo intento

--- Efectos/Delay.cs
+++ Efectos/Delay.cs
@@ -10,6 +10,9 @@ namespace Efectos
     class Delay : ISampleProvider
     {
         int offsetTiempoMS;
+
+        List<float> muestras = new List<float>();
+
         private ISampleProvider fuente;
 
 
@@ -30,19 +33,30 @@ namespace Efectos
         //Offset, numero de muestras leídas
         public int Read(float[] buffer, int offset, int count)
         {
+
+            
+
             var read = fuente.Read(buffer, offset, count);
-            float tiempoTranscurrido = (float)offset / (float)fuente.WaveFormat.SampleRate;
+            float tiempoTranscurrido = 
+                (float)muestras.Count / (float)fuente.WaveFormat.SampleRate;
 
             float tiempoTranscurridoMS = tiempoTranscurrido * 1000;
-            int numMuestrasOffsetTiempo = (int)(((float)offsetTiempoMS / 1000.0f) * (float)fuente.WaveFormat.SampleRate);
+            int numMuestrasOffsetTiempo = 
+                (int)(((float)offsetTiempoMS / 1000.0f) * (float)fuente.WaveFormat.SampleRate);
 
             if (tiempoTranscurridoMS > offsetTiempoMS)
             {
                 for (int i = 0; i < read; i++)
                 {
-                    buffer[offset + 1] += buffer[offset+i-numMuestrasOffsetTiempo];
+                    buffer[offset + 1] += muestras[muestras.Count+i-numMuestrasOffsetTiempo];
                 }
             }
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                muestras.Add(buffer[i]);
+            }
+
             return read;
         }
     }
